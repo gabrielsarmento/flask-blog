f2c6163843632064787cefb31ea78aab5fb45104
Gabriel Sarmento <gabrielfs.bot@gmail.com>
2018-11-29 19:09:11 -0300

    Configurando DB url

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -3,7 +3,7 @@ url = 'postgres://postgres:@localhost:5432/postgres'
 
 class Config:
     SECRET_KEY = 'okgoogle'
-    SQLALCHEMY_DATABASE_URI = os.environ['url']
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
